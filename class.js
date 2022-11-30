e544bfa34eee117fb4e5260c966903eb11e0865b
Christian Ani <iamchristianani@gmail.com>
2022-11-30 12:04:36 +0100

    Add event listeners for list and add book sections

--- class.js
+++ class.js
@@ -5,6 +5,12 @@ const bookInfo = document.querySelector('.all-books');
 const title = document.querySelector('#input-title');
 const author = document.querySelector('#input-author');
 const dateEl = document.querySelector('.date');
+const listBtn = document.querySelector('.list-button');
+const addBtn = document.querySelector('.add-button');
+const contactBtn = document.querySelector('.contact-button');
+const bookSection = document.querySelector('.books-list');
+const addBookSection = document.querySelector('.add-book-form');
+const contactSection = document.querySelector('.contact-info');
 
 let bookArr = [];
 
@@ -66,3 +72,21 @@ window.addEventListener('load', () => {
 // SPA Navigation
 const date = new Date();
 dateEl.innerHTML = date;
+
+listBtn.addEventListener('click', () => {
+  bookSection.classList.add('active');
+  bookSection.classList.remove('hiiden');
+  addBookSection.classList.add('hidden');
+  addBookSection.classList.remove('active');
+  contactSection.classList.add('hidden');
+  contactSection.classList.remove('active');
+});
+
+addBtn.addEventListener('click', () => {
+  bookSection.classList.remove('active');
+  bookSection.classList.add('hidden');
+  addBookSection.classList.add('active');
+  addBookSection.classList.remove('hidden');
+  contactSection.classList.add('hidden');
+  contactSection.classList.remove('active');
+});
